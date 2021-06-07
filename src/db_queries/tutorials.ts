05ff6c93113eb0162ff35b4dcb284058f03cc547
Edmond Pruteanu <edmondpr@gmail.com>
2021-06-07 18:40:17 +0300

    Added index to tutorial queries

--- src/db_queries/tutorials.ts
+++ src/db_queries/tutorials.ts
@@ -14,7 +14,7 @@ export class Tutorials {
 
   async getTutorials(request: Request, response: Response): Promise<void> {
     try {
-      const getTutorialsQuery = 'SELECT * FROM tutorials';
+      const getTutorialsQuery = 'SELECT * FROM tutorials ORDER BY index';
       const { rows }: pg.QueryResult = await pool.query(getTutorialsQuery);
       const tutorials: Array<Tutorial> = [];
       for (const row of rows) {
@@ -31,7 +31,9 @@ export class Tutorials {
   }
 
   async getTutorialSections(id: string): Promise<Array<string>> {
-    const getTutorialSectionsQuery = 'SELECT * FROM tutorials_sections WHERE tutorial_id = $1';
+    const getTutorialSectionsQuery = `SELECT * FROM tutorials_sections 
+      WHERE tutorial_id = $1
+      ORDER BY index`;
     const { rows }: pg.QueryResult = await pool.query(getTutorialSectionsQuery, [id]);
     const sections: Array<string> = [];
     for (const row of rows) {
