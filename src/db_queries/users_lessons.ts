29b5680ff0e867463ff18335a19ef534499ef37e
Edmond Pruteanu <edmondpr@gmail.com>
2021-06-23 22:43:15 +0300

    Removed timezone in recurrence

--- src/db_queries/users_lessons.ts
+++ src/db_queries/users_lessons.ts
@@ -60,10 +60,10 @@ export class UsersLessons {
     const now = moment.tz(new Date(), timeZone?.name);
     console.log('now: ' + now.format(constants.DATE_TIME_FORMAT));
     if (lessonRow) {
-      const endRecurrenceDateTime = moment.tz(lessonRow.end_recurrence_date_time, timeZone?.name);
+      const endRecurrenceDateTime = moment(lessonRow.end_recurrence_date_time);
       console.log('endRecurrenceDateTime: ' + endRecurrenceDateTime);
       console.log('endRecurrenceDateTime formatted: ' + endRecurrenceDateTime.format(constants.DATE_TIME_FORMAT));
-      let lessonDateTime = moment.tz(lessonRow.date_time, timeZone?.name);
+      let lessonDateTime = moment(lessonRow.date_time);
       console.log('lessonDateTime: ' + lessonDateTime);
       console.log('lessonDateTime formatted: ' + lessonDateTime.format(constants.DATE_TIME_FORMAT));
       if (lessonRow.is_recurrent) {
