bc15d8006a02500b7501ed0a62f9558c1724fdc3
Edmond Pruteanu <edmondpr@gmail.com>
2021-06-26 19:08:47 +0300

    Separated date time for next and previous lesson

--- src/db_queries/users_lessons.ts
+++ src/db_queries/users_lessons.ts
@@ -54,44 +54,48 @@ export class UsersLessons {
     let lessonRow = rows[0];
     let students: Array<User> = [];
     if (lessonRow) {
-      let lesson: Lesson = {
+      const lesson: Lesson = {
         dateTime: moment.utc(lessonRow.date_time).format(constants.DATE_TIME_FORMAT),
         isRecurrent: lessonRow.is_recurrent
       }
       if (lessonRow.end_recurrence_date_time != null) {
         lesson.endRecurrenceDateTime = moment.utc(lessonRow.end_recurrence_date_time).format(constants.DATE_TIME_FORMAT)
       }
-      lesson = this.checkLessonRecurrence(lesson);
-      if (Object.keys(lesson).length === 0) {
+      const lessonDateTime = this.getNextLessonDateTime(lesson);
+      if (lessonDateTime == undefined) {
         lessonRow = null;
       } else {
+        lessonRow.date_time = lessonDateTime;
         students = await this.getLessonStudents(lessonRow.id);
       }
     }
     return this.setLesson(lessonRow, students, isMentor);
   }
 
-  checkLessonRecurrence(lesson: Lesson): Lesson {
+  getNextLessonDateTime(lesson: Lesson): string | undefined {
     const now = moment.utc();
     const endRecurrenceDateTime = moment.utc(lesson.endRecurrenceDateTime);
     let lessonDateTime = moment.utc(lesson.dateTime);
     if (lesson.isRecurrent) {
       if (endRecurrenceDateTime.isBefore(now)) {
-        return {};
+        return undefined;
       } else {
         while (lessonDateTime.isBefore(now)) {
           lessonDateTime = lessonDateTime.add(7, 'd');
         }
         if (lessonDateTime.isAfter(endRecurrenceDateTime)) {
-          return {};
+          return undefined;
+        } else {
+          return moment.utc(lessonDateTime).format(constants.DATE_TIME_FORMAT);
         }
       }
     } else {
       if (lessonDateTime.isBefore(now)) {
-        return {};
+        return undefined;
+      } else {
+        return moment.utc(lessonDateTime).format(constants.DATE_TIME_FORMAT);
       }
     }
-    return lesson;    
   }
 
   async getLessonStudents(lessonId: string): Promise<Array<User>> {
@@ -162,7 +166,7 @@ export class UsersLessons {
           FROM users_lessons ul
           JOIN subfields s
           ON ul.subfield_id = s.id
-          WHERE mentor_id = $1 AND ul.date_time::timestamp < $2
+          WHERE ul.mentor_id = $1 AND ul.date_time::timestamp < $2
           ORDER BY ul.date_time DESC LIMIT 1`;
       } else {
         getPreviousLessonQuery = `SELECT ul.id, ul.mentor_id, ul.subfield_id, ul.date_time, s.name AS subfield_name, ul.meeting_url, ul.is_recurrent, ul.end_recurrence_date_time, ul.is_canceled
@@ -175,17 +179,54 @@ export class UsersLessons {
           ORDER BY ul.date_time DESC LIMIT 1`;      
       }
       const { rows }: pg.QueryResult = await pool.query(getPreviousLessonQuery, [userId, now]);
-      const lessonRow = rows[0];
+      let lessonRow = rows[0];
       let students: Array<User> = [];
+      let lesson: Lesson = {};
       if (lessonRow != null) {
-        students = await this.getLessonStudents(lessonRow.id);
+        lesson = {
+          dateTime: moment.utc(lessonRow.date_time).format(constants.DATE_TIME_FORMAT),
+          isRecurrent: lessonRow.is_recurrent
+        }
+        if (lessonRow.end_recurrence_date_time != null) {
+          lesson.endRecurrenceDateTime = moment.utc(lessonRow.end_recurrence_date_time).format(constants.DATE_TIME_FORMAT);
+        }
+        const lessonDateTime = this.getPreviousLessonDateTime(lesson);
+        if (lessonDateTime == undefined) {
+          lessonRow = null;
+        } else {
+          lessonRow.date_time = lessonDateTime;
+          students = await this.getLessonStudents(lessonRow.id);
+        }
       }
-      const lesson = await this.setLesson(lessonRow, students, isMentor);
+      lesson = await this.setLesson(lessonRow, students, isMentor);
       response.status(200).json(lesson);
     } catch (error) {
       response.status(400).send(error);
     }
-  }  
+  }
+  
+  getPreviousLessonDateTime(lesson: Lesson): string | undefined {
+    const now = moment.utc();
+    const endRecurrenceDateTime = moment.utc(lesson.endRecurrenceDateTime);
+    let lessonDateTime = moment.utc(lesson.dateTime);
+    if (lesson.isRecurrent) {
+      while (lessonDateTime.isBefore(endRecurrenceDateTime)) {
+        lessonDateTime = lessonDateTime.add(7, 'd');
+      }
+      lessonDateTime = lessonDateTime.subtract(7, 'd');
+      if (endRecurrenceDateTime.isAfter(now)) {
+        if (lessonDateTime.isBefore(now)) {
+          return moment.utc(lessonDateTime).format(constants.DATE_TIME_FORMAT);
+        } else {
+          return undefined;
+        }
+      } else {
+        return moment.utc(lessonDateTime).format(constants.DATE_TIME_FORMAT);
+      } 
+    } else {
+      return moment.utc(lessonDateTime).format(constants.DATE_TIME_FORMAT);
+    } 
+  }     
 
   async cancelLesson(request: Request, response: Response): Promise<void> {
     const lessonId: string = request.params.id;
