b1cc9bac5d66d1923af03b7250e4cc7d539d1e48
Edmond Pruteanu <edmondpr@gmail.com>
2021-10-04 21:11:52 +0300

    Fixed timezone abbreviation when sent correctly

--- src/db_queries/users_timezones.ts
+++ src/db_queries/users_timezones.ts
@@ -34,17 +34,21 @@ export class UsersTimeZones {
 
   async addTimeZone(userId: string, timeZone: TimeZone, client: pg.PoolClient): Promise<void> {
     const insertTimeZoneQuery = `INSERT INTO users_timezones (user_id, abbreviation, name, utc_offset) VALUES ($1, $2, $3, $4)`;
-    const timezoneAbbreviation = moment.tz(timeZone.name).zoneAbbr();
+    const timezoneAbbreviation = this.getTimeZoneAbbreviation(timeZone.name, timeZone.abbreviation);
     const values = [userId, timezoneAbbreviation, timeZone.name, timeZone.offset];        
     await client.query(insertTimeZoneQuery, values);
   }
 
+  getTimeZoneAbbreviation(timeZoneName: string, abbreviation: string): string {
+    return abbreviation != null && abbreviation.indexOf('+') > -1 ? moment.tz(timeZoneName).zoneAbbr() : abbreviation;
+  }
+
   async updateTimeZone(request: Request, response: Response): Promise<void> {
     const userId = request.user.id as string;
-    const { name, offset }: TimeZone = request.body
+    const { name, abbreviation, offset }: TimeZone = request.body
     try {
       const updateTimeZoneQuery = 'UPDATE users_timezones SET name = $1, abbreviation = $2, utc_offset = $3 WHERE user_id = $4';
-      const timezoneAbbreviation = moment.tz(name).zoneAbbr();
+      const timezoneAbbreviation = this.getTimeZoneAbbreviation(name, abbreviation);
       await pool.query(updateTimeZoneQuery, [name, timezoneAbbreviation, offset, userId]);
       response.status(200).send(`User timezone modified with ID: ${userId}`);
     } catch (error) {
