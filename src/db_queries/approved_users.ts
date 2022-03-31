662469c997c51070d244644da41250ec945058e7
Edmond Pruteanu <edmondpr@gmail.com>
2022-03-31 23:38:36 +0300

    Check if approved user exists

--- src/db_queries/approved_users.ts
+++ src/db_queries/approved_users.ts
@@ -42,36 +42,44 @@ export class ApprovedUsers {
   }  
 
   async addApprovedUser(request: Request, response: Response): Promise<void> {
-    const { email, organization, isMentor }: ApprovedUser = request.body    
+    const { email, whatsappNumber, organization, isMentor }: ApprovedUser = request.body    
     const client = await pool.connect();
     try {
       await client.query('BEGIN');
-      if (organization) {
-        if (!organization?.name) {
+      let approvedUserExists = false;
+      const getApprovedUsersQuery = 'SELECT id FROM approved_users WHERE email = $1';
+      let { rows }: pg.QueryResult = await client.query(getApprovedUsersQuery, [email]);
+      if (rows[0]) {
+        approvedUserExists = true;
+      }
+      if (!approvedUserExists) {
+        if (organization) {
           organization.id = '1838ae4e-4d07-4b03-8c7e-e1aa07fa9835'; // Other organization
-        } else {
           const getOrganizationQuery = 'SELECT id FROM organizations WHERE name = $1';
-          const { rows }: pg.QueryResult = await client.query(getOrganizationQuery, [organization.name]);
+          ({ rows } = await client.query(getOrganizationQuery, [organization.name]));
           if (rows[0]) {
             organization.id = rows[0].id;
           }
         }
-      }
-      const fieldId = 'b021984a-c02c-4fd4-87a7-1aec84c68d6b'; // Programming
-      const goal = 'I want to have an income of at least $1000 USD per month';
-      let insertApprovedUserQuery;
-      let values;
-      if (isMentor) {
-        insertApprovedUserQuery = `INSERT INTO approved_users (email, field_id, organization_id, is_mentor)
+        let insertApprovedUserQuery;
+        let values;
+        if (isMentor) {
+          const fieldId = 'b021984a-c02c-4fd4-87a7-1aec84c68d6b'; // Programming
+          insertApprovedUserQuery = `INSERT INTO approved_users (email, field_id, organization_id, is_mentor)
           VALUES ($1, $2, $3, $4)`;
-        values = [email, fieldId, organization?.id, isMentor];
+          values = [email, fieldId, organization?.id, isMentor];
+        } else {
+          const fieldId = '62a9dce6-2e92-45cc-a580-035d64a65b5b'; // Other
+          const goal = 'I want to have an income of at least $1000 USD per month';
+          insertApprovedUserQuery = `INSERT INTO approved_users (email, whatsapp_number, field_id, organization_id, is_mentor, goal)
+            VALUES ($1, $2, $3, $4, $5, $6)`;
+          values = [email, whatsappNumber, fieldId, organization?.id, isMentor, goal];        
+        }
+        await client.query(insertApprovedUserQuery, values);
+        response.status(200).send('Approved user has been added');
       } else {
-        insertApprovedUserQuery = `INSERT INTO approved_users (email, organization_id, is_mentor, goal)
-          VALUES ($1, $2, $3, $4)`;
-        values = [email, organization?.id, isMentor, goal];        
+        response.status(200).send('Approved user already exists');
       }
-      await client.query(insertApprovedUserQuery, values);
-      response.status(200).send('Approved user has been added');
       await client.query('COMMIT');
     } catch (error) {
       response.status(400).send(error);
