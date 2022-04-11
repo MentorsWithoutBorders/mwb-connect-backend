5db4df0df30bc0ebf51dfcf69cef8ec2d6cecd8f
Edmond Pruteanu <edmondpr@gmail.com>
2022-04-11 21:35:30 +0300

    Test WhatsApp

--- server.ts
+++ server.ts
@@ -44,16 +44,9 @@ const whatsAppClient = new Client({
   puppeteer: {
     headless: true,
     args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--disable-gpu'
+      '--no-sandbox'
     ]
-  },
-  authStrategy: new LocalAuth()  
+  } 
 });
 const port = process.env.PORT;
 const app: express.Express = express();
