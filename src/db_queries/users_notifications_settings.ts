4467721940d8c775c58e6c9f329f6e5fddbeebc1
Edmond Pruteanu <edmondpr@gmail.com>
2023-05-01 03:08:08 +0300

    Fixed notifications compatibility

--- src/db_queries/users_notifications_settings.ts
+++ src/db_queries/users_notifications_settings.ts
@@ -26,11 +26,23 @@ export class UsersNotificationsSettings {
     try {
       const getNotificationsSettingsQuery = 'SELECT enabled, time, training_reminders_enabled, training_reminders_time, start_course_reminders_enabled, start_course_reminders_date FROM users_notifications_settings WHERE user_id = $1';
       const { rows }: pg.QueryResult = await pool.query(getNotificationsSettingsQuery, [userId]);
-      const trainingRemindersTime = rows[0].training_reminders_time.substring(0, rows[0].training_reminders_time.lastIndexOf(':'));
+			let trainingRemindersEnabledDB = true;
+			if (rows[0].training_reminders_enabled) {
+				trainingRemindersEnabledDB = rows[0].training_reminders_enabled;
+			} else if (rows[0].enabled) {
+				trainingRemindersEnabledDB = rows[0].enabled;
+			}
+			let trainingRemindersTimeDB = '';
+			if (rows[0].training_reminders_time) {
+				trainingRemindersTimeDB = rows[0].training_reminders_time;
+			} else if (rows[0].time) {
+				trainingRemindersTimeDB = rows[0].time;
+			}
+      const trainingRemindersTime = trainingRemindersTimeDB.substring(0, trainingRemindersTimeDB.lastIndexOf(':'));
       const notificationsSettings: NotificationsSettings = {
-				enabled: rows[0].enabled,
-				time: rows[0].time,
-        trainingRemindersEnabled: rows[0].training_reminders_enabled,
+				enabled: trainingRemindersEnabledDB,
+				time: trainingRemindersTime,
+        trainingRemindersEnabled: trainingRemindersEnabledDB,
         trainingRemindersTime: trainingRemindersTime,
 				startCourseRemindersEnabled: rows[0].start_course_reminders_enabled,
 				startCourseRemindersDate: rows[0].start_course_reminders_date ? moment.utc(rows[0].start_course_reminders_date).format(constants.DATE_TIME_FORMAT) : null
@@ -51,11 +63,23 @@ export class UsersNotificationsSettings {
         SET enabled = $1, time = $2, training_reminders_enabled = $3, training_reminders_time = $4, start_course_reminders_enabled = $5, start_course_reminders_date = $6 WHERE user_id = $7`;
 			const user = await users.getUserFromDB(userId, client);
 			const userTimeZone = await usersTimeZones.getUserTimeZone(userId, client);
+			let trainingRemindersEnabledForUpdate = true;
+			if (trainingRemindersEnabled) {
+				trainingRemindersEnabledForUpdate = trainingRemindersEnabled;
+			} else if (enabled) {
+				trainingRemindersEnabledForUpdate = enabled;
+			}
+			let trainingRemindersTimeForUpdate = '';
+			if (trainingRemindersTime) {
+				trainingRemindersTimeForUpdate = trainingRemindersTime;
+			} else if (time) {
+				trainingRemindersTimeForUpdate = time;
+			}
 			let values = [];
 			if (user.isMentor) {
-				values = [enabled, time, trainingRemindersEnabled, trainingRemindersTime, startCourseRemindersEnabled, moment.utc(startCourseRemindersDate).tz(userTimeZone.name).format(constants.DATE_FORMAT), userId];
+				values = [trainingRemindersEnabledForUpdate, trainingRemindersTimeForUpdate, trainingRemindersEnabledForUpdate, trainingRemindersTimeForUpdate, startCourseRemindersEnabled, moment.utc(startCourseRemindersDate).tz(userTimeZone.name).format(constants.DATE_FORMAT), userId];
 			} else {
-				values = [trainingRemindersEnabled, trainingRemindersTime, null, null, userId];
+				values = [trainingRemindersEnabledForUpdate, trainingRemindersTimeForUpdate, trainingRemindersEnabledForUpdate, trainingRemindersTimeForUpdate, null, null, userId];
 			}
       await client.query(updateNotificationsSettingsQuery, values);
       usersSendEmails.sendEmailNotificationsSettingsUpdate(userId, trainingRemindersEnabled);
