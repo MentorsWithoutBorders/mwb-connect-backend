d30b6e9e9d605cf5d56cb1cd60930bc25e3cbb75
Manoj Bahuguna <manojbahugunamanoj@gmail.com>
2023-07-14 10:12:29 +0530

    chore: remove user fields from approved_user model to prevent duplication

--- src/models/approved_user.model.ts
+++ src/models/approved_user.model.ts
@@ -2,7 +2,4 @@ import User from "./user.model";
 
 export default interface ApprovedUser extends User {
   goal?: string;
-  isMentor?: boolean;
-  isOrgManager?: boolean;
-  isCentreManager?: boolean;
 }
